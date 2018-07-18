b41fb39f398fb45ed6c2c47286a963fbac9206f1
Brian Cheung <bcheung.comp@gmail.com>
2018-07-18 18:09:01 -0500

    twilio integration

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,18 @@
-module.exports = function() {
+const admin = require('firebase-admin');
+
+module.exports = function(req, res) {
+  // Verify user provided phone
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+
+  // Format phone number to remove dashes and parens
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
   
+  // Create new user account using phone number
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
+
+  // Respond to the user request, saying the account was made
 }
